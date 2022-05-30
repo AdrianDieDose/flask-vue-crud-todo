0163b8e62b5e6293fa4205d69ff8ed4845591c5a
Adrian Duda <adrianduda2019@gmail.com>
2022-05-30 12:34:24 +0200

    Added modal/popup for posting

--- client/src/components/ToDo.vue
+++ client/src/components/ToDo.vue
@@ -5,7 +5,7 @@
         <h1>To Do App</h1>
         <hr />
         <br /><br />
-        <button type="button" class="btn btn-success btn-sm">Add Task</button>
+        <button type="button" class="btn btn-success btn-sm" v-b-modal.todo-modal>Add Task</button>
         <br /><br />
         <table class="table table-hover">
           <thead>
@@ -35,6 +35,38 @@
         </table>
       </div>
     </div>
+
+    <b-modal ref="addToDoModal" id="todo-modal" title="Add a new ToDo" hide-footer>
+      <b-form @submit="onSubmit" @reset="onReset" class="w-100">
+        <b-form-group id="form-task-group" label="Task:" label-for="form-task-input">
+          <b-form-input
+            id="form-task-input"
+            type="text"
+            v-model="addToDoForm.task"
+            required
+            placeholder="Enter task"
+          >
+          </b-form-input>
+        </b-form-group>
+        <b-form-group id="form-author-group" label="Author:" label-for="form-author-input">
+          <b-form-input
+            id="form-author-input"
+            type="text"
+            v-model="addToDoForm.author"
+            required
+            placeholder="Enter author"
+          >
+          </b-form-input>
+        </b-form-group>
+        <b-form-group id="form-done-group">
+          <b-form-checkbox-group v-model="addToDoForm.done" id="form-checks">
+            <b-form-checkbox value="true">Done?</b-form-checkbox>
+          </b-form-checkbox-group>
+        </b-form-group>
+        <b-button type="submit" variant="primary">Submit</b-button>
+        <b-button type="reset" variant="danger">Reset</b-button>
+      </b-form>
+    </b-modal>
   </div>
 </template>
 
@@ -46,6 +78,11 @@ export default {
   data() {
     return {
       todo: [],
+      addToDoForm: {
+        task: '',
+        author: '',
+        done: [],
+      },
     };
   },
   methods: {
@@ -60,6 +97,42 @@ export default {
           // eslint-disable-next-line
           console.error(error);
         });
+    },
+    addToDo(payload) {
+      const path = 'http://localhost:5000/todo';
+      axios
+        .post(path, payload)
+        .then(() => {
+          this.getToDo();
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.log(error);
+          this.getToDo();
+        });
+    },
+    initForm() {
+      this.addToDoForm.task = '';
+      this.addToDoForm.author = '';
+      this.addToDoForm.done = [];
+    },
+    onSubmit(evt) {
+      evt.preventDefault();
+      this.$refs.addToDoModal.hide();
+      let read = false;
+      if (this.addToDoForm.done[0]) read = true;
+      const payload = {
+        task: this.addToDoForm.task,
+        author: this.addToDoForm.author,
+        read, // property shorthand
+      };
+      this.addToDo(payload);
+      this.initForm();
+    },
+    onReset(evt) {
+      evt.preventDefault();
+      this.$refs.addToDoModal.hide();
+      this.initForm();
     },
   },
   created() {
